f21e776298fc9da0f96d7b0b9f457f543c91be26
Star Sander <sanderstar@creetion.com>
2019-06-12 14:04:02 +0000

    First version

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,18 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("opensap.movies.controller.App", {
+		onInit: function () {
+
+		},
+		
+		onPress: function (oEvent) {
+			sap.ui.require(["sap/m/MessageToast"], function (oMessage) {
+				oMessage.show("Searching...");
+			});
+		}	
+
+	});
+});
